37c593ab8fc96274f103f051b36f0c1d802cb4d8
mennasaid247 <mennasaid247@gmail.com>
2024-09-19 18:32:33 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// let js="amazing"
+// if(js==='amazing')
+// alert("hiiii");
+// console.log( 40+5-10);
+
+// let js=12;
+// console.log(typeof js);
+
+
+
+let markMass,markHeight,markBMI;
+let johnMass,johnHeight,johnBMI;
+markMass= 78;
+markHeight=1.69;
+johnMass=92;
+johnHeight=1.95;
+markBMI=markMass/(markHeight*markHeight);
+johnBMI=johnMass/(johnHeight*johnHeight);
+const markHigherBMI=markBMI>johnBMI;
+console.log(markBMI,johnBMI,markHigherBMI);
+console.log("hiiiiiii");
+console.log(`markBMI is ${markBMI} and johnBMI is ${johnBMI} and markHigherBMI is ${markHigherBMI}`);
